13758efe66350d21c8532f3630a0ab23510900cf
강동윤 <yty455@naver.com>
2023-08-09 03:26:08 +0900

    progress: intent 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'dart:convert';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
 Future<void> myBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
@@ -16,6 +19,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage(myBackgroundHandler);
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: '6715e7eb3fcb15643b5ddf2c35bd52d1',
+    javaScriptAppKey: '94f660d5b29760bb7ff5e51729ad26be',
+  );
   runApp(const MaterialApp(home: MyApp(),));
 }
 
@@ -67,6 +75,16 @@ class _MyAppState extends State<MyApp> {
 
   void sendFcmTokenToWeb(String token){
     inAppWebViewController.evaluateJavascript(source: 'receiveFcmToken("${json.encode(token)}");');
+  }
+
+  static const methodChannel = MethodChannel('Channel Name');
+  bool isAppLink(Uri url) {
+    final appScheme = url.scheme;
+
+    return appScheme != 'http' &&
+        appScheme != 'https' &&
+        appScheme != 'about:blank' &&
+        appScheme != 'data';
   }
 
   @override
@@ -133,6 +151,14 @@ class _MyAppState extends State<MyApp> {
 
     flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
+  Future<void> _launchKotlinActivity() async {
+    const platform = MethodChannel('com.example.push_notification_fcm_example/MainActivity');
+    try {
+      await platform.invokeMethod('openKotlinActivity');
+    } catch (e) {
+      print("Error invoking Kotlin activity: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -157,14 +183,18 @@ class _MyAppState extends State<MyApp> {
                 URLRequest(url: Uri.parse("http://43.200.254.50:80")),
                 onWebViewCreated: (InAppWebViewController controller) {
                   callPermissions();
+                  _launchKotlinActivity();
                   inAppWebViewController = controller;
                   // 자바스크립트 채널 연결
                   inAppWebViewController.addJavaScriptHandler(handlerName: 'handleFoo', callback: (args) { print("나 왔어"); return{'fcmT':fcmToken};});
                 },
                 initialOptions: InAppWebViewGroupOptions(
                     crossPlatform: InAppWebViewOptions(
+                        javaScriptEnabled: true,
+                        javaScriptCanOpenWindowsAutomatically: true,
                         useShouldOverrideUrlLoading: true,
                         verticalScrollBarEnabled: true,
+                        useShouldInterceptFetchRequest: true,
                         userAgent: 'Mozilla/5.0 (Linux; Android 9; LG-H870 Build/PKQ1.190522.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36'
                     ),
                 ),
@@ -176,7 +206,73 @@ class _MyAppState extends State<MyApp> {
                       resources: resources,
                       action: PermissionRequestResponseAction.GRANT);
                 },
-
+                // shouldOverrideUrlLoading: (controller, navigationAction) async {
+                //   var uri = navigationAction.request.url;
+                //
+                //   if (uri.scheme == 'intent') {
+                //     // "intent:" URL 처리 로직
+                //     // ...
+                //     return ShouldOverrideUrlLoadingAction.CANCEL;
+                //   }
+                //
+                //   // 나머지 서비스 로직 구현
+                //
+                //   return ShouldOverrideUrlLoadingAction.ALLOW;
+                // },
+                // shouldOverrideUrlLoading: (controller, navigationAction) async {
+                //
+                //   print('====================shouldOverrideUrlLoading====================');
+                //   var curUrl = navigationAction.request.url;
+                //
+                //   if (curUrl == null) return NavigationActionPolicy.CANCEL;
+                //
+                //   // URL String의 Shceme가 http, https 가 아닌 지 검거
+                //   if (isAppLink(curUrl)) {
+                //     await controller.stopLoading();
+                //
+                //     var scheme = curUrl.scheme;
+                //
+                //     if (scheme == "intent") {        // 일반적인 Intent의 경우
+                //       if (Platform.isAndroid) {
+                //         try {
+                //           final parsedIntent = await methodChannel.invokeMethod('getAppUrl', {'url': curUrl.toString()});
+                //           print(parsedIntent);
+                //
+                //           if (await canLaunchUrl(Uri.parse(parsedIntent))) {
+                //             launchUrl(parsedIntent);
+                //           } else {
+                //             final marketUrl = await methodChannel.invokeMethod('getMarketUrl', {'url': curUrl.toString()});
+                //             launchUrl(marketUrl);
+                //           }
+                //         } on PlatformException catch (e) {
+                //           // 오류 처리
+                //           print('${e.message}');
+                //         }
+                //       }
+                //     } else if (scheme.contains('snssdk')) {    // TikTok 인경우 이미 파싱이 되어진 상태로 넘어온다. 따라서, 실행가능여부 검사 후 실행 및 패키지 다운로드로 진행
+                //       if (Platform.isAndroid) {
+                //         try {
+                //           if (await canLaunchUrl(curUrl)) {
+                //             launchUrl(curUrl.toString());
+                //           } else {
+                //             launchUrl('market://details?id=com.ss.android.ugc.trill');
+                //           }
+                //         } on PlatformException catch (e) {
+                //           // 오류 처리
+                //           print('${e.message}');
+                //         }
+                //       }
+                //     }
+                //
+                //     return NavigationActionPolicy.CANCEL;
+                //   } else {
+                //     return NavigationActionPolicy.ALLOW;
+                //   }
+                //
+                //
+                //   print(
+                //       '====================shouldOverrideUrlLoading====================');
+                // },
                 onProgressChanged:
                     (InAppWebViewController controller, int progress) {
                   setState(() {
